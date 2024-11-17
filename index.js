6a688c7b6c6eafe12446f4cb149553b1fcdbc211
abeni-al7 <abenezeralebachew3@gmail.com>
2024-11-17 21:36:26 +0300

    Prepare for deployment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hobbyRoutes = require('./routes/hobbyRoutes');
 const dreamRoutes = require('./routes/dreamRoutes');
 
 const app = express();
+require('dotenv').config();
 
 app.use(express.json());
 app.use(nameRoutes);
@@ -12,6 +13,11 @@ app.use(hobbyRoutes);
 app.use(dreamRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.ENVIRONMENT === 'development') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+} else {
+    module.exports = app;
+}
