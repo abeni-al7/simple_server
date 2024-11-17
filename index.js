3696d1b8db51fbdefb2e83e22e4fb320cd7cad2e
abeni-al7 <abenezeralebachew3@gmail.com>
2024-11-17 20:19:37 +0300

    SETUP development environment

---
+++ index.js
@@ -0,0 +1,10 @@
+// Entry point for server
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
